637c169a8cf11769643c87827c5c3138693cea60
Finn <finn@finn.io>
2015-07-05 12:36:14 -0700

    Trying to check batches of 10 instead of every TLD

--- public/js/check.js
+++ public/js/check.js
@@ -1,7 +1,36 @@
+function checkNames(names) {
+  $.get('/check/' + names.join(","), function(data) {
+    if(data.ApiResponse.CommandResponse[0].DomainCheckResult !== undefined) {
+      data.ApiResponse.CommandResponse[0].DomainCheckResult.forEach(function(result) {
+        var splitbydots = result.$.Domain.split(".", 2);
+        var name = splitbydots[0];
+        var TLD = splitbydots[1];
+        if(splitbydots.length > 2) {
+          for(var i = 2; i < splitbydots.length; i++) {
+            TLD += "." + splitbydots[i];
+          }
+        }
+
+        var box = $("." + TLD).text(result.$.Domain);
+        if(result.$.Available == "true") {
+          box.removeClass("unknown").addClass("available");
+        } else if(result.$.Available == "false") {
+          box.removeClass("unknown").addClass("unavailable");
+        } else {
+          console.log("whut", result);
+        }
+      });
+    } else {
+      console.error("data.ApiResponse.CommandResponse[0].DomainCheckResult is undefined", data);
+    }
+  });
+}
 $(document).ready(function() {
-  $.get('/tlds', function(tlds) {
-    for(var tld in tlds) {
-      if(tlds.hasOwnProperty(tld)) {
+  var tlds = [];
+  $.get('/tlds', function(tldlist) {
+    tlds = tldlist;
+    for(var tld in tldlist) {
+      if(tldlist.hasOwnProperty(tld)) {
         var box = $("<span>")
           .addClass('tld')
           .addClass('unknown')
@@ -13,31 +42,13 @@ $(document).ready(function() {
   });
   $('#name').on('blur', function(evt) {
     var name = evt.currentTarget.value;
-    console.log('Looking up', name);
-    $.get('/check/' + name, function(data) {
-      if(data.ApiResponse.CommandResponse[0].DomainCheckResult !== undefined) {
-        data.ApiResponse.CommandResponse[0].DomainCheckResult.forEach(function(result) {
-          var splitbydots = result.$.Domain.split(".", 2);
-          var name = splitbydots[0];
-          var TLD = splitbydots[1];
-          if(splitbydots.length > 2) {
-            for(var i = 2; i < splitbydots.length; i++) {
-              TLD += "." + splitbydots[i];
-            }
-          }
-
-          var box = $("." + TLD).text(result.$.Domain);
-          if(result.$.Available == "true") {
-            box.removeClass("unknown").addClass("available");
-          } else if(result.$.Available == "false") {
-            box.removeClass("unknown").addClass("unavailable");
-          } else {
-            console.log("whut", result);
-          }
-        });
-      } else {
-        console.error("data.ApiResponse.CommandResponse[0].DomainCheckResult is undefined", data);
+    for(var i = 0; i < tlds.length; i++) {
+      var names = [];
+      for(var j = 0; j + i < tlds.length && j-10 < i; j++) {
+        names.push[i + j];
       }
-    });
+      checkNames(names);
+    }
+    console.log('Looking up', name);
   });
 });
