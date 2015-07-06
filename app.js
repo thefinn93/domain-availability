494c3598b307df2f3bc4dce8bf804b1479e2d61a
Finn <finn@finn.io>
2015-07-05 20:47:28 -0700

    favicon

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ app.use(raven.middleware.express.errorHandler(config.dsn));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
