66d855b25af6d2a9a6cbd47ff11fe7cba98ec3d3
Finn <finn@finn.io>
2015-07-05 19:47:00 -0700

    Forgot to include config on the index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Domain Availability Checker' });
+  res.render('index', { title: 'Domain Availability Checker', config: config });
 });
 
 module.exports = router;
