d635e5f8ef4ce80f9c3d891715549586ea91e9ae
Finn <finn@finn.io>
2015-07-05 17:17:50 -0700

    updated routes to match what frontend has been updated to be like

--- routes/check.js
+++ routes/check.js
@@ -1,46 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var config = require('../config.json');
 var ncapi = require('../ncapi');
-var Q = require('q');
-var getTLDs = require('../TLDs');
 
-
-function addTLDs(name) {
-  var deferred = Q.defer();
-  getTLDs().then(function(TLDs) {
-    var out = [];
-    for(var tld in TLDs) {
-      if(TLDs.hasOwnProperty(tld)) {
-        out.push(name + '.' + tld);
-      }
-    }
-    deferred.resolve(out.join(","));
-  }).catch(function(err) {
-    deferred.reject(err);
-  });
-  return deferred.promise;
-}
-
-function checkName(name) {
-  var deferred = Q.defer();
-  addTLDs(name).then(function(names) {
-    console.log("Checking", names);
-    ncapi('namecheap.domains.check', {'DomainList': names}).then(function(result) {
-      deferred.resolve(result);
-    }).catch(function(err) {
-      console.log(err.stack || err);
-      deferred.reject(err);
-    });
-  }).catch(function(err) {
-    console.log(err.stack || err);
-    deferred.reject(err);
-  });
-  return deferred.promise;
-}
-
-router.get('/:name', function(req, res, next) {
-  checkName(req.params.name).then(function(result) {
+router.get('/:names', function(req, res, next) {
+  ncapi('namecheap.domains.check', {'DomainList': req.params.names}).then(function(result) {
     res.json(result);
   }).catch(function(err) {
     next(err);
