e510eb8630544e2f7ab63b3aac6e6e2304ff3349
Finn <finn@finn.io>
2015-07-06 00:30:45 -0700

    Added analytics. Fixes #3

--- routes/settings.js
+++ routes/settings.js
@@ -14,11 +14,27 @@ router.get('', function(req, res, next) {
       out.push("Raven.config('" + config.dsn + "', " + JSON.stringify(ravenconfig) + ").install();");
     });
   }
+
   if(config.NCaffiliateID) {
-    out.push("var aff = " + config.NCaffiliateID);
+    out.push("var aff = " + config.NCaffiliateID + ";");
   } else {
-    out.push("var aff = \"\"");
+    out.push("var aff = \"\";");
   }
+
+  if(config.piwik && config.piwik.uri && config.piwik.site) {
+    var trackingCode = "var _paq = _paq || [];";
+    trackingCode += "_paq.push([\"trackPageView\"]);";
+    trackingCode += "_paq.push([\"enableLinkTracking\"]);";
+    trackingCode += "(function() {";
+    trackingCode += "var u=\"" + config.piwik.uri + "/\";";
+    trackingCode += "_paq.push([\"setTrackerUrl\", u+\"piwik.php\"]);";
+    trackingCode += "_paq.push([\"setSiteId\", \"" + config.piwik.site + "\"]);";
+    trackingCode += "var d=document, g=d.createElement(\"script\"), s=d.getElementsByTagName(\"script\")[0]; g.type=\"text/javascript\";";
+    trackingCode += "g.defer=true; g.async=true; g.src=u+\"piwik.js\"; s.parentNode.insertBefore(g,s);";
+    trackingCode += "})();";
+    out.push(trackingCode);
+  }
+  res.set('Content-Type', 'application/javascript');
   res.send(out.join("\n"));
 });
 
