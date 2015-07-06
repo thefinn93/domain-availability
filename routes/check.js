4bb32c2fede98002f1e6b9f8dae04651e4fad668
Finn <finn@finn.io>
2015-07-05 22:53:32 -0700

    Send client IP

--- routes/check.js
+++ routes/check.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var ncapi = require('../ncapi');
+var config = require('../config.json');
+
+function getClientIP(req) {
+  var clientIP = req.connection.remoteAddress || '127.0.0.1';
+  var trustedProxies = ['127.0.0.1'];
+  var header = 'X-Forwarded-For';
+  if(config.proxies) {
+    trustedProxies = config.proxies;
+  }
+  if(config.realIPheader) {
+    header = config.realIPheader;
+  }
+  if(proxies.indexOf(req.connection.remoteAddress) != -1) {
+    clientIP = req.headers[header] || req.connection.remoteAddress;
+  }
+  return clientIP;
+}
 
 router.get('/:names', function(req, res, next) {
-  ncapi({Command: 'namecheap.domains.check', DomainList: req.params.names}).then(function(result) {
+  ncapi({Command: 'namecheap.domains.check', DomainList: req.params.names, ClientIP: getClientIP(req)}).then(function(result) {
     res.json(result);
   }).catch(function(err) {
     next(err);
