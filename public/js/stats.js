dd46bd71dd090dbb3fe511a88d6214d7d721e0c4
Finn <finn@finn.io>
2015-07-07 20:37:04 -0700

    better stats

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,3 +1,10 @@
+function avg(input) {
+  var output = 0;
+  input.forEach(function(i) {
+    output += i;
+  });
+  return Math.round(output/input.length)/1000;
+}
 $(document).ready(function() {
   var stats = [];
   timings.forEach(function(line) {
@@ -5,13 +12,10 @@ $(document).ready(function() {
       var data = line.split(",");
       var size = parseInt(data[0], 10);
       var time = parseInt(data[1], 10);
-      if(size < stats.length) {
-        stats.length = size+1;
-      }
       if(stats[size] === undefined) {
-        stats[size] = 0;
+        stats[size] = [];
       }
-      stats[size]++;
+      stats[size].push(time);
     }
   });
   var keys = [];
@@ -19,12 +23,13 @@ $(document).ready(function() {
   for(var i = 0; i < stats.length; i++) {
     if(stats[i] !== undefined) {
       keys.push(i);
-      values.push(stats[i]);
+      values.push(avg(stats[i]));
+      console.log(i, stats[i]);
     }
   }
 
   var ctx = $(".results")[0].getContext("2d");
-  new Chart(ctx).Bar({
+  var data = {
     labels: keys,
     datasets: [
       {
@@ -36,5 +41,7 @@ $(document).ready(function() {
         data: values
       }
     ]
-  });
+  };
+  console.log(data);
+  new Chart(ctx).Bar(data);
 });
