81488c59ed63c57dc9aa6384530cddbaa1fe5f46
Finn Herzfeld <finn@finn.io>
2018-03-02 15:07:56 -0800

    does that even work?

--- confighandler.js
+++ confighandler.js
@@ -1,6 +1,6 @@
 var debug = require('debug')('domain-availability:config');
 
 var configfile = process.env.CONFIG || __dirname + '/config.json'
-debug('Loading config from ' + configfile)
+debug('Loading config from ', configfile)
 module.exports = require(configfile);
-debug("Config file: " + module.exports);
+debug("Config file: ", module.exports);
