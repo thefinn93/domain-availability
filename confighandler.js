71d7aa1b4f2b1e305f2be63fe88217d485278201
Finn Herzfeld <finn@finn.io>
2018-03-02 15:05:25 -0800

    Learned how debug works

--- confighandler.js
+++ confighandler.js
@@ -3,6 +3,4 @@ var debug = require('debug')('domain-availability:config');
 var configfile = process.env.CONFIG || __dirname + '/config.json'
 debug('Loading config from ' + configfile)
 module.exports = require(configfile);
-if(process.env.DEBUG_CONFIG == "true") {
-  debug("Config file: " + module.exports);
-}
+debug("Config file: " + module.exports);
